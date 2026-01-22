237743696542f5c3b86b8e41484f6758b596b759
sudamgalande9822-hub <sudamgalande9822@gmail.com>
2026-01-22 19:58:19 +0530

    Add student class with initialization and display method

--- first.py
+++ first.py
@@ -1,2 +1,7 @@
-print("hello world welcome to python programming ")
-print("welocome to programming")
+class student:
+  def __init__(self,name):
+    self.name = name 
+  def show(self):
+    print(f"name = {self.name}") 
+c = student("sudam") 
+c.show()
