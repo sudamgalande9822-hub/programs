05ede206b8dcb717f642d7eafffe1a4f16c71e6c
sudamgalande9822-hub <sudamgalande9822@gmail.com>
2026-01-24 20:13:27 +0530

    performing some changes

--- second.py
+++ second.py
@@ -379,3 +379,214 @@ def add(x,y):
 l = [11,22,33,44,55]
 res = reduce(add,l)
 print(res)"""
+
+
+# Reverse a list.
+"""l = [11,22,33,44,55]
+rev = []
+for i in l:
+    rev.insert(0,i)
+print("rev = ",rev)  """  
+
+# Find the largest number in a list.
+"""l = [11,22,33,44,55]
+print(max(l))"""
+
+# Find the smallest number in a list.
+"""l = [11,22,33,44,55] 
+print(min(l))"""
+
+# Count elements in a list.
+"""l = [11,22,33,44,55] 
+count = 0 
+for i in l:
+    count += 1 
+print(count)  """  
+# Check if a number exists in a list.
+"""l = [11,22,33,44,55]
+if 11 in l:
+    print("yes") 
+else:
+    print("not")"""
+
+# Sum of all elements in a list.
+"""l = [11,22,33,44,55]
+sum = 0 
+for i in l:
+    sum += i 
+print("sum = ",sum)  """  
+
+# Remove duplicates from a list.
+"""l = [11,11,22,33,11,22,33] 
+l1 = []
+for i in l:
+    if i not in l1:
+        l1.append(i) 
+print(l1) """     
+
+# Count even and odd numbers in a list.
+"""
+l = [11,22,33,44,55]
+even = 0 
+odd = 0 
+for i in l:
+    if i % 2 == 0:
+        even += 1 
+    else:
+        odd += 1 
+print("even = ",even) 
+print("odd = ",odd)"""           
+
+# Find second largest number in a list.
+"""
+l = [11,22,33,44,55]
+print("max = ",l[-2])"""
+
+# Swap first and last elements of a list. 
+"""l = [11,22,33,44,55] 
+l[0],l[-1] = l[-1],l[0] 
+print(l)"""
+
+# Check if a list is a palindrome.
+"""l = [11,22,33,22,11] 
+if l == l[::-1]:
+    print("palindrome")
+else:
+    print("not palindrome")   """ 
+
+# Find common elements between two lists.
+"""
+l = [11,22,33,44,55]
+l1 = [11,33,66,77]
+print(set(l) & set(l1))"""
+
+# Find missing number in a list of 1 to n.
+"""def missing_numbers(nums):
+    n = len(nums) + 1 
+    expected_sum = n * (n+1)//2  
+    actual_sum = sum(nums) 
+    return expected_sum - actual_sum 
+print(missing_numbers([1,3,4,5]))"""
+# Rotate a list by k positions.
+"""l = [11, 22, 33, 44]
+k = 22
+
+k = k % len(l)
+
+l = l[-k:] + l[:-k]
+print(l)"""
+
+# Split a list into chunks.
+
+# Flatten a nested list.
+
+# Find duplicates in a list.
+"""l = [11,22,33,44,11,22,33]
+original = []
+duplicates = [] 
+for i in l:
+    if i in original:
+        duplicates.append(i) 
+    else: 
+        original.append(i)  
+print(duplicates)"""           
+
+# Sort a list without using sort().
+"""l = [1,2,3,1,2,6] 
+pos = 0 
+for i in l:
+    if i != 0:
+        l[]"""
+
+# Find frequency of each element.
+"""l = [11,22,33,44,55]
+freq = {} 
+for i in l:
+    if i in freq:
+        freq[i]+=1 
+    else:
+        freq[i]=1 
+print("freq = ",freq) """           
+# Merge two sorted lists.
+
+"""l = [11,22,33,44,55]
+l1 = [1,2,1,4,5] 
+print(sorted(l1+l))"""
+
+# Create a tuple and print its elements.
+"""t = (11,22,33,44,55)
+for i in t:
+    print(i)"""
+
+# Find the length of a tuple.
+"""
+t = (11,22,33,44,55)
+len = 0 
+for i in t:
+    len += 1 
+print(len) """   
+
+# Access the first and last element of a tuple.
+"""t = (11,22,33,44,55)
+print("first = ", t[0])
+print("last = ",t[4])"""
+
+# Convert a tuple into a list.
+"""t = (11,22,33,44,55) 
+print(list(t))
+"""
+# Convert a list into a tuple.
+"""t = [11,22,33,44,55]
+print(tuple(t))
+"""
+# Check if an element exists in a tuple.
+"""t = (11,22,33,44,55)
+if 11 in t:
+    print("present")
+else:
+    print("not present") """ 
+
+# Count occurrences of an element in a tuple.
+"""t = (11,22,33,11,22,33,11,11,111,111,11) 
+count = 0 
+for i in t:
+    if i == 11:
+        count += 1 
+print("Count = ",count)        
+"""
+# Find the index of an element in a tuple.
+"""t = (11,22,33,44,55)
+print(t[-1])
+print(t[1])"""
+
+# Find the maximum and minimum element in a tuple.
+
+"""t = (11,22,33,44,66,77)
+min_val = t[0] 
+max_val = t[0]
+for i in t:
+       if i < min_val:
+              min_val = i 
+       if i > max_val:
+              max_val = i 
+print("min_val = ",min_val)
+print("max_val = ",max_val) """                    
+# Reverse a tuple.
+"""
+s = (11,22,33,44,55)
+t = []
+for i in s:
+       t.insert(0,i)
+print(t)       """
+
+# Sort a tuple.
+
+# Concatenate two tuples.
+
+# Remove duplicate elements from a tuple.
+
+# Unpack a tuple into variables.
+
+# Convert a tuple of strings into integers.
+
+# Find sum of elements in a tuple.
